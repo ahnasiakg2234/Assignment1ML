83cae5f5b3d26548537a4b7a1b8ff5d29f38209a
ahnasiakg2234 <ahnasia04@gmail.com>
2026-02-10 16:06:19 -1000

    Assignment 1

--- decisiontree.py
+++ decisiontree.py
@@ -1,8 +1,10 @@
 
 import sklearn
+from sklearn import tree
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
-
+from sklearn.tree import plot_tree
+import matplotlib.pyplot as plt
 
 breast_cancer = datasets.load_breast_cancer()
 X = breast_cancer.data
@@ -13,9 +15,12 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 decision_tree = sklearn.tree.DecisionTreeClassifier()
 decision_tree.fit(X_train, y_train)
 
+tree.plot_tree(decision_tree)
+plt.show()
+
+
 accuracy = decision_tree.score(X_test, y_test)
 print("Accuracy:", accuracy)
-
 
 precision = sklearn.metrics.precision_score(y_test, decision_tree.predict(X_test))
 print("Precision:", precision)
